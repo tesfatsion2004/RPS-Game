7d0a3360c95f1dfb459df0606f558024d1bb82e6
tesfatsion2004 <tesfatsion2004@gmail.com>
2020-05-28 17:37:13 +0300

    Delete tempe.js

--- tempe.js
+++
@@ -1,5 +0,0 @@
-let src;
-        if (lang_script && lang_script.getAttribute('src')!=null && lang_script.getAttribute('src').indexOf(oldfilename)!=-1)
-        {
-            src= lang_script.getAttribute('src').indexOf()!=-1;
-        }
